843a67077dd713fecd46d751d0025e418d2cf1ca
liyanbo <liyanbo@insta360.com>
2021-10-03 22:06:44 +0800

    add mysql class

---
+++ mysql.py
@@ -0,0 +1,100 @@
+import pymysql.cursors
+import time
+
+
+class Mysql:
+    def __init__(self, mysql_host, mysql_port, mysql_username, mysql_password, mysql_db, table_name, device_id):
+        self.mysql_host = mysql_host
+        self.mysql_port = mysql_port
+        self.mysql_username = mysql_username
+        self.mysql_password = mysql_password
+        self.mysql_db = mysql_db
+        self.table_name = table_name
+        self.device_id = device_id
+        
+    def db_connect(self):
+        # 根据mysql配置参数 返回游标对象
+        connect = pymysql.Connect(
+            host=self.mysql_host,
+            port=int(self.mysql_port),
+            user=self.mysql_username,
+            passwd=self.mysql_password,
+            db=self.mysql_db,
+            charset='utf8'
+        )
+        # 获取连接对象
+        return connect
+
+    def db_init(self):
+        try:
+            connect = self.db_connect()
+            # 获取游标
+            cursor = connect.cursor()
+            cursor.execute("use test;")
+            cursor.execute("CREATE TABLE my_fps_{} (fps VARCHAR(255), jank VARCHAR(255), big_jank  VARCHAR(255), "
+                           "stutter  VARCHAR(255), time timestamp)".format(self.table_name))
+            cursor.execute("CREATE TABLE my_gpu_{} (gpu_Device VARCHAR(255), gpu_Renderer VARCHAR(255), "
+                           "gpu_Tiler VARCHAR(255), time  timestamp)".format(self.table_name))
+            cursor.execute(
+                "CREATE TABLE my_cpu_{} (use_cpu VARCHAR(255), sys_cpu VARCHAR(255), count_cpu VARCHAR(255), "
+                "time  timestamp)".format(self.table_name))
+            cursor.execute("CREATE TABLE my_memory_{} (memory VARCHAR(255), time  timestamp)".format(self.table_name))
+            time.sleep(3)
+            connect.commit()
+            # 关闭连接
+            cursor.close()
+            connect.close()
+            
+        except BaseException as e:
+            print("Error: " + str(e))
+    
+    def insert_cpu(self, value):
+        cpu_sql_prefix = "INSERT INTO my_cpu_{} (use_cpu) VALUES('".format(self.table_name)
+        sql = cpu_sql_prefix + value + "')"
+        connect = self.db_connect()
+        cursor = connect.cursor()
+        cursor.execute(sql)
+        connect.commit()
+        # 关闭连接
+        cursor.close()
+        connect.close()
+
+    def insert_memory(self, value):
+        mem_sql_prefix = "INSERT INTO my_memory_{} (memory) VALUES('".format(self.table_name)
+        sql = mem_sql_prefix + value + "')"
+        connect = self.db_connect()
+        cursor = connect.cursor()
+        cursor.execute(sql)
+        connect.commit()
+        # 关闭连接
+        cursor.close()
+        connect.close()
+
+    def insert_fps(self, fps, jank, big_jank, stutter):
+        fps_sql_prefix = "INSERT INTO my_fps_{} (fps,jank,big_jank,stutter) VALUES('".format(self.table_name)
+        sql = fps_sql_prefix + fps + "','" + jank + "','" + big_jank + "','" + stutter + "')"
+        connect = self.db_connect()
+        cursor = connect.cursor()
+        cursor.execute(sql)
+        connect.commit()
+        # 关闭连接
+        cursor.close()
+        connect.close()
+
+    def insert_gpu(self, gpu_device, gpu_renderer, gpu_tiler):
+        gpu_sql_prefix = "INSERT INTO my_gpu_{} (gpu_Device,gpu_Renderer,gpu_Tiler) VALUES('".format(self.table_name)
+        sql = gpu_sql_prefix + gpu_device + "','" + gpu_renderer + "','" + gpu_tiler + "')"
+        connect = self.db_connect()
+        cursor = connect.cursor()
+        cursor.execute(sql)
+        connect.commit()
+        # 关闭连接
+        cursor.close()
+        connect.close()
+
+
+
+
+
+
+
--- run.py
+++ run.py
@@ -7,6 +7,7 @@ modified by liyanbo at 20210927
 import ast
 import datetime
 import json
+import os
 import time
 import webbrowser
 from pprint import pprint
@@ -14,193 +15,9 @@ import argparse
 import pymysql.cursors
 import requests
 import tidevice
+from grafana import Grafana
+from mysql import Mysql
 from ios_device import py_ios_device
-import gradio as gr
-
-
-fps_sql_prefix = ""
-cpu_sql_prefix = ""
-gpu_sql_prefix = ""
-mem_sql_prefix = ""
-
-
-def add_mysql_source():
-    base_url = 'http://{}:{}@{}:{}/api/'.format(grafana_username, grafana_password, grafana_host, grafana_port)
-    url = base_url + 'datasources'
-    response = requests.get(url)
-    if 'mysql' not in response.content.decode():
-        body = {
-            "name": "MySQL",
-            "type": "mysql",
-            "url": "db",
-            "password": "admin",
-            "user": "root",
-            "database": "test",
-            "access": "proxy"
-        }
-        response = requests.post(url=url, data=body)
-        if response.status_code == 200:
-            print("add datasource MySQL success")
-        else:
-            print("Error Code：" + str(response.status_code))
-            print("Error Message: \n" + str(response.content.decode()))
-
-
-def db_connect(host, port, user, pw, db):
-    # 接受mysql配置参数 返回游标对象
-    connect = pymysql.Connect(
-        host=host,
-        port=int(port),
-        user=user,
-        passwd=pw,
-        db=db,
-        charset='utf8'
-    )
-    # 获取游标
-    return connect
-
-
-def db_init(table_name):
-    try:
-        global fps_sql_prefix, cpu_sql_prefix, gpu_sql_prefix, mem_sql_prefix
-        fps_sql_prefix = "INSERT INTO my_fps_{} (fps,jank,big_jank,stutter) VALUES('".format(table_name)
-        cpu_sql_prefix = "INSERT INTO my_cpu_{} (use_cpu) VALUES('".format(table_name)
-        gpu_sql_prefix = "INSERT INTO my_gpu_{} (gpu_Device,gpu_Renderer,gpu_Tiler) VALUES('".format(table_name)
-        mem_sql_prefix = "INSERT INTO my_memory_{} (memory) VALUES('".format(table_name)
-        connect = db_connect(mysql_host, mysql_port, mysql_username, mysql_password, mysql_db)
-        # 获取游标
-        cursor = connect.cursor()
-        cursor.execute("use test;")
-        cursor.execute("CREATE TABLE my_fps_{} (fps VARCHAR(255), jank VARCHAR(255), big_jank  VARCHAR(255), "
-                       "stutter  VARCHAR(255), time timestamp)".format(table_name))
-        cursor.execute("CREATE TABLE my_gpu_{} (gpu_Device VARCHAR(255), gpu_Renderer VARCHAR(255), "
-                       "gpu_Tiler VARCHAR(255), time  timestamp)".format(table_name))
-        cursor.execute("CREATE TABLE my_cpu_{} (use_cpu VARCHAR(255), sys_cpu VARCHAR(255), count_cpu VARCHAR(255), "
-                       "time  timestamp)".format(table_name))
-        cursor.execute("CREATE TABLE my_memory_{} (memory VARCHAR(255), time  timestamp)".format(table_name))
-    except BaseException as e:
-        print("Error: " + str(e))
-
-
-def grafana_setup(table_name):
-    add_mysql_source()
-    panels_list = [
-        {'aliasColors': {}, 'bars': False, 'dashLength': 10, 'dashes': False, 'datasource': 'MySQL', 'fill': 1,
-         'fillGradient': 0, 'gridPos': {'h': 8, 'w': 12, 'x': 0, 'y': 0}, 'hiddenSeries': False, 'id': 8,
-         'legend': {'avg': False, 'current': False, 'max': False, 'min': False, 'show': True, 'total': False,
-                    'values': False}, 'lines': True, 'linewidth': 1, 'nullPointMode': 'null',
-         'options': {'dataLinks': []}, 'percentage': False, 'pointradius': 2, 'points': False, 'renderer': 'flot',
-         'seriesOverrides': [], 'spaceLength': 10, 'stack': False, 'steppedLine': False, 'targets': [
-            {'format': 'time_series', 'group': [], 'metricColumn': 'none', 'rawQuery': True,
-             'rawSql': 'SELECT\n  $__timeGroupAlias(time,3s),\n  avg(memory) AS "MEM"\nFROM my_memory_{}\nWHERE\n '
-                       ' $__timeFilter(time)\nGROUP BY 1\nORDER BY $__timeGroup(time,3s)'.format(table_name),
-             'refId': 'A', 'select': [[{'params': ['value'], 'type': 'column'}]], 'timeColumn': 'time',
-             'where': [{'name': '$__timeFilter', 'params': [], 'type': 'macro'}]}], 'thresholds': [], 'timeFrom': None,
-         'timeRegions': [], 'timeShift': None, 'title': 'MEM',
-         'tooltip': {'shared': True, 'sort': 0, 'value_type': 'individual'}, 'type': 'graph',
-         'xaxis': {'buckets': None, 'mode': 'time', 'name': None, 'show': True, 'values': []},
-         'yaxes': [{'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True},
-                   {'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True}],
-         'yaxis': {'align': False, 'alignLevel': None}},
-        {'aliasColors': {}, 'bars': False, 'dashLength': 10, 'dashes': False, 'datasource': 'MySQL', 'fill': 1,
-         'fillGradient': 0, 'gridPos': {'h': 8, 'w': 12, 'x': 12, 'y': 0}, 'hiddenSeries': False, 'id': 6,
-         'legend': {'avg': False, 'current': False, 'max': False, 'min': False, 'show': True, 'total': False,
-                    'values': False}, 'lines': True, 'linewidth': 1, 'nullPointMode': 'null',
-         'options': {'dataLinks': []}, 'percentage': False, 'pointradius': 2, 'points': False, 'renderer': 'flot',
-         'seriesOverrides': [], 'spaceLength': 10, 'stack': False, 'steppedLine': False, 'targets': [
-            {'format': 'time_series', 'group': [], 'metricColumn': 'none', 'rawQuery': True,
-             'rawSql': 'SELECT\n  $__timeGroupAlias(time,3s),\n  avg(fps) AS "FPS"\nFROM my_fps_{}\nWHERE\n '
-                       ' $__timeFilter(time)\nGROUP BY 1\nORDER BY $__timeGroup(time,3s)'.format(table_name),
-             'refId': 'A', 'select': [[{'params': ['value'], 'type': 'column'}]], 'timeColumn': 'time',
-             'where': [{'name': '$__timeFilter', 'params': [], 'type': 'macro'}]}], 'thresholds': [], 'timeFrom': None,
-         'timeRegions': [], 'timeShift': None, 'title': 'FPS',
-         'tooltip': {'shared': True, 'sort': 0, 'value_type': 'individual'}, 'type': 'graph',
-         'xaxis': {'buckets': None, 'mode': 'time', 'name': None, 'show': True, 'values': []},
-         'yaxes': [{'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True},
-                   {'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True}],
-         'yaxis': {'align': False, 'alignLevel': None}},
-        {'aliasColors': {}, 'bars': False, 'dashLength': 10, 'dashes': False, 'datasource': 'MySQL', 'fill': 1,
-         'fillGradient': 0, 'gridPos': {'h': 9, 'w': 12, 'x': 0, 'y': 8}, 'hiddenSeries': False, 'id': 2,
-         'legend': {'avg': False, 'current': False, 'max': False, 'min': False, 'show': True, 'total': False,
-                    'values': False}, 'lines': True, 'linewidth': 1, 'nullPointMode': 'null',
-         'options': {'dataLinks': []}, 'percentage': False, 'pointradius': 2, 'points': False, 'renderer': 'flot',
-         'seriesOverrides': [], 'spaceLength': 10, 'stack': False, 'steppedLine': False, 'targets': [
-            {'format': 'time_series', 'group': [], 'metricColumn': 'none', 'rawQuery': True,
-             'rawSql': 'SELECT\n  $__timeGroupAlias(time,3s),\n  avg(gpu_Device) AS "GPU"\nFROM my_gpu_{}\nWHERE\n '
-                       ' $__timeFilter(time)\nGROUP BY 1\nORDER BY $__timeGroup(time,3s)'.format(table_name),
-             'refId': 'A', 'select': [[{'params': ['value'], 'type': 'column'}]], 'timeColumn': 'time',
-             'where': [{'name': '$__timeFilter', 'params': [], 'type': 'macro'}]}], 'thresholds': [], 'timeFrom': None,
-         'timeRegions': [], 'timeShift': None, 'title': 'GPU',
-         'tooltip': {'shared': True, 'sort': 0, 'value_type': 'individual'}, 'type': 'graph',
-         'xaxis': {'buckets': None, 'mode': 'time', 'name': None, 'show': True, 'values': []},
-         'yaxes': [{'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True},
-                   {'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True}],
-         'yaxis': {'align': False, 'alignLevel': None}},
-        {'aliasColors': {}, 'bars': False, 'dashLength': 10, 'dashes': False, 'datasource': 'MySQL', 'fill': 1,
-         'fillGradient': 0, 'gridPos': {'h': 8, 'w': 12, 'x': 12, 'y': 8}, 'hiddenSeries': False, 'id': 4,
-         'legend': {'avg': False, 'current': False, 'max': False, 'min': False, 'show': True, 'total': False,
-                    'values': False}, 'lines': True, 'linewidth': 1, 'nullPointMode': 'null',
-         'options': {'dataLinks': []}, 'percentage': False, 'pointradius': 2, 'points': False, 'renderer': 'flot',
-         'seriesOverrides': [], 'spaceLength': 10, 'stack': False, 'steppedLine': False, 'targets': [
-            {'format': 'time_series', 'group': [], 'metricColumn': 'none', 'rawQuery': True,
-             'rawSql': 'SELECT\n  $__timeGroupAlias(time,3s),\n  avg(use_cpu) AS "CPU"\nFROM my_cpu_{}\nWHERE\n  '
-                       '$__timeFilter(time)\nGROUP BY 1\nORDER BY $__timeGroup(time,3s)'.format(table_name),
-             'refId': 'A', 'select': [[{'params': ['value'], 'type': 'column'}]], 'timeColumn': 'time',
-             'where': [{'name': '$__timeFilter', 'params': [], 'type': 'macro'}]}], 'thresholds': [], 'timeFrom': None,
-         'timeRegions': [], 'timeShift': None, 'title': 'CPU',
-         'tooltip': {'shared': True, 'sort': 0, 'value_type': 'individual'}, 'type': 'graph',
-         'xaxis': {'buckets': None, 'mode': 'time', 'name': None, 'show': True, 'values': []},
-         'yaxes': [{'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True},
-                   {'format': 'short', 'label': None, 'logBase': 1, 'max': None, 'min': None, 'show': True}],
-         'yaxis': {'align': False, 'alignLevel': None}}]
-    data = '''
-    {
-      "dashboard": {
-        "id": null,
-        "uid": null,
-        "title": "%s",
-
-        "timezone": "browser",
-        "timepicker": {
-            "refresh_intervals": [
-              "1s",
-              "5s",
-              "10s",
-              "30s",
-              "1m",
-              "5m",
-              "15m",
-              "30m"
-            ]
-          },
-        "panels": %s  ,
-        "templating": {
-        "list": []
-      },
-        "annotations": {
-        "list": []
-      },
-        "schemaVersion": 16,
-        "version": 2
-      },
-      "folderId": 0,
-      "overwrite": true
-    }
-    ''' % (table_name, str(json.dumps(panels_list)))
-    pprint(data)
-
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.post(url="http://{}:{}@{}:{}/api/dashboards/db".format(grafana_username, grafana_password,
-                            grafana_host, grafana_port), data=data, headers=headers)
-    print(response)
-    print(response.text)
-    response_dict = ast.literal_eval(response.text)
-    grafana_url = "http://{}:{}".format(grafana_host, grafana_port) + response_dict['url'] + "?orgId=1&refresh=1s&from=now-5m&to=now"
-    webbrowser.open(grafana_url)
 
 
 def callback_fps(res):
@@ -212,16 +29,7 @@ def callback_fps(res):
     big_jank = ss.split("'big_jank':")[1].split(",")[0]
     stutter = ss.split("'stutter':")[1][0:5].split("}")[0]
     # 数据存数据库连接数据库
-    connect = db_connect(mysql_host, mysql_port, mysql_username, mysql_password, mysql_db)
-
-    # 获取游标
-    cursor = connect.cursor()
-    sql = fps_sql_prefix + fps_test + "','" + jank_test + "','" + big_jank + "','" + stutter + "')"
-    cursor.execute(sql)
-    connect.commit()
-    # 关闭连接
-    cursor.close()
-    connect.close()
+    mysql.insert_fps(fps_test, jank_test, big_jank, stutter)
 
 
 def callback_gpu(res):
@@ -232,17 +40,7 @@ def callback_gpu(res):
     gpu_Renderer = ss.split("'Renderer Utilization %':")[1].split(",")[0]
     gpu_Tiler = ss.split("'Tiler Utilization %':")[1].split(",")[0]
     # 数据存数据库连接数据库
-    connect = db_connect(mysql_host, mysql_port, mysql_username, mysql_password, mysql_db)
-
-    # 获取游标
-    cursor = connect.cursor()
-    sql = gpu_sql_prefix + gpu_Device + "','" + gpu_Renderer + "','" + gpu_Tiler + "')"
-    cursor.execute(sql)
-    connect.commit()
-    # print('GPU成功插入', cursor.rowcount, '条数据')
-    # 关闭连接
-    cursor.close()
-    connect.close()
+    mysql.insert_gpu(gpu_Device, gpu_Renderer, gpu_Tiler)
 
 
 def start_test():
@@ -258,31 +56,13 @@ def start_test():
             ss = str(value)  # 转成str
             use_cpu = ss.split("'value':")[1][0:6].split("}")[0]
             # 数据存数据库连接数据库
-            connect = db_connect(mysql_host, mysql_port, mysql_username, mysql_password, mysql_db)
-
-            # 获取游标
-            cursor = connect.cursor()
-            sql = cpu_sql_prefix + use_cpu + "')"
-            cursor.execute(sql)
-            connect.commit()
-            # 关闭连接
-            cursor.close()
-            connect.close()
+            mysql.insert_cpu(use_cpu)
         if _type.value == "memory":
             print('内存打印', value)
             ss = str(value)
             memory = ss.split("'value':")[1][0:6].split("}")[0]
             # 数据存数据库连接数据库
-            connect = db_connect(mysql_host, mysql_port, mysql_username, mysql_password, mysql_db)
-
-            # 获取游标
-            cursor = connect.cursor()
-            sql = mem_sql_prefix + memory + "')"
-            cursor.execute(sql)
-            connect.commit()
-            # 关闭连接
-            cursor.close()
-            connect.close()
+            mysql.insert_memory(memory)
 
     perf.start(app_bundle_id, callback=callback)
     time.sleep(99999)  # 测试时长
@@ -294,6 +74,7 @@ def start_test():
 
 
 if __name__ == "__main__":
+
     print("wait for mysql setup")
     # time.sleep(10)
     parser = argparse.ArgumentParser()
@@ -326,6 +107,14 @@ if __name__ == "__main__":
     mysql_db = args.mysql_db
 
     table_name = tidevice.Device(device_id).name + "_" + datetime.datetime.now().strftime("%m%d_%H%M")
-    db_init(table_name)
-    grafana_setup(table_name)
+
+    mysql = Mysql(mysql_host, mysql_port, mysql_username, mysql_password, mysql_db, table_name, device_id)
+    mysql.db_init()
+
+    grafana = Grafana(grafana_host, grafana_port, grafana_username, grafana_password, mysql_host, mysql_port,
+                      mysql_username, mysql_password, mysql_db, table_name, device_id)
+    grafana.add_mysql_source()
+    grafana.setup_dashboard()
+    grafana.to_explorer()
+
     start_test()
